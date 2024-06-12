b824dc4fa8f5272bebfe9cb016a3a18e1208873a
sunshine-web-coder <webdev4gaming@gmail.com>
2024-06-12 04:11:00 +0100

    first commit

---
+++ src/components/mockData/MockData.ts
@@ -0,0 +1,81 @@
+import { Account } from "@/utils/types";
+
+export const mockAccounts: Account[] = [
+  {
+    account_id: 'ACC001',
+    holder: {
+      firstName: 'James',
+      lastName: 'Radford',
+      jointNames: 'Anthony Adrian & Thomas Work',
+      username: 'sample1',
+      password: 'sample1',
+    },
+    bank_details: {
+      bank_name: 'Bank of Springfield',
+      branch: 'Main Branch',
+      account_number: '23232932732',
+      routing_number: '623623636',
+      swift_code: 'BOSUS33',
+      account_type: 'Checking',
+      balance_usd: 100500.23,
+    },
+    transaction_history: [
+      {
+        transaction_id: 'TXN001',
+        dateTime: 'March 23rd 2024, 3:32:32 am',
+        description: "Sinlair's Store - Trx",
+        status: "Success",
+        amount_usd: 452.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: 'TXN002',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'Justin Loci',
+        status: "Success",
+        amount_usd: 1152.21,
+        account_no: "****7676",
+      },
+      {
+        transaction_id: 'TXN003',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'Transfer',
+        status: "Success",
+        amount_usd: -100.0,
+        account_no: "****4409",
+      },
+      {
+        transaction_id: 'TXN004',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'Payment',
+        status: "Success",
+        amount_usd: -50.0,
+        account_no: "****5093",
+      },
+      {
+        transaction_id: 'TXN005',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'Online Purchase',
+        status: "Success",
+        amount_usd: -75.0,
+        account_no: "****2334",
+      },
+      {
+        transaction_id: 'TXN006',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'ATM Withdrawal',
+        status: "Success",
+        amount_usd: -150.0,
+        account_no: "****3213",
+      },
+      {
+        transaction_id: 'TXN007',
+        dateTime: 'March 20th 2024, 2:02:22 am',
+        description: 'Transfer',
+        status: "Pending",
+        amount_usd: 200.0,
+        account_no: "****4434",
+      },
+    ],
+  },
+];
